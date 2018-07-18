6965cbb00d8bcf725f3d53128d57e70fd00774c8
austintackaberry <austintackaberry@gmail.com>
2018-07-18 07:56:56 -0700

    Add transition

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -3,7 +3,7 @@ const minHand = document.querySelector(".min-hand");
 const secondHand = document.querySelector(".second-hand");
 
 const clockSetter = () => {
-    let currentDate = new Date(new Date().getTimezoneOffset() + Date.now());
+    const currentDate = new Date(new Date().getTimezoneOffset() + Date.now());
     let hours = currentDate.getHours() + 3;
     let mins = currentDate.getMinutes() + 15;
     let seconds = currentDate.getSeconds() + 15;
@@ -12,9 +12,10 @@ const clockSetter = () => {
     hours = hours > 24 ? hours - 12 : hours;
     hours = hours > 12 ? hours - 12 : hours;
 
-    secondHand.style.transform = "rotate(" + seconds / 60 + "turn)";
-    minHand.style.transform = "rotate(" + mins / 60 + "turn)";
-    hourHand.style.transform = "rotate(" + hours / 12 + "turn)";
+    secondHand.style.transform = `rotate(${seconds / 60}turn)`;
+    minHand.style.transform = `rotate(${mins / 60}turn)`;
+    hourHand.style.transform = `rotate(${hours / 12}turn)`;
+
     return setTimeout(clockSetter, 1000);
 };
 
